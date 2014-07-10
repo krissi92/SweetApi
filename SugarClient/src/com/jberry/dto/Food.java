8f7f916357f60d8e42b750d8fe9a53d982fbeb76
olingi <regnbogasulta@gmail.com>
2014-07-10 13:09:12 +0000

    added food stuff

---
+++ SugarClient/src/com/jberry/dto/Food.java
@@ -0,0 +1,34 @@
+package com.jberry.dto;
+
+
+//impliments?
+public class Food {
+    int id;
+    String Nafn;
+    String Fæðuflokkur;
+    float Prótein;
+    float Fita;
+    float trans-fita;
+    float Kólesteról;
+    float Kolvetni;
+    float Sykur;
+    float Alkohól;
+    float Vatn;
+    float A-vítamín;
+    float D-vítamín;
+    float E-vítamín;
+    float B1-vítamín;
+    float B2-vítamín;
+    float B6-vítamín;
+    float B12-vítamín;
+    float C-vítamín;
+    float Kalk;
+    float Fosfor;
+    float Magnesíum;
+    float Járn;
+    float Sink;
+    float Natríum;
+    int Kalíum;
+    float Kopar;
+    float Joð;
+}
