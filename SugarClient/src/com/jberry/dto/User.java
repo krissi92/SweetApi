59436639090151e8384a11006d0f61fac7a0364a
krissi92 <jayflug@gmail.com>
2014-07-15 15:57:47 +0000

    made an artifact, dno wat to do wit et

--- SugarClient/src/com/jberry/dto/User.java
+++ SugarClient/src/com/jberry/dto/User.java
@@ -3,8 +3,8 @@ package com.jberry.dto;
 
 //impliments?
 public class User {
-    String userName = "admin";
-    String password = "admin";
-    String email = "a@a.is";
-    int Id;
+    public String userName = "admin";
+    public String password = "admin";
+    public String email = "a@a.is";
+    public int Id;
 }
--- SugarClient/src/com/jberry/services/user/UserServiceImpl.java
+++ SugarClient/src/com/jberry/services/user/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.jberry.services.user;
 
+
 import java.util.List;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -12,7 +13,7 @@ import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
+//import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 
 import com.jberry.dto.User;
@@ -22,6 +23,12 @@ public class UserServiceImpl implements UserService {
 	@Override
 	public boolean login(String username, String password) throws Exception {
 		// TODO Auto-generated method stub
+        User Admun = new User();
+        if(Admun.userName == username && Admun.password == password)
+        {
+            return true;
+        }
+
         /*String Url = "http://localhost:3000";
         HttpClient client = new DefaultHttpClient();
         HttpGet request = new HttpGet(url);
