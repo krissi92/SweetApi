4c6f295f1686e00cb314f45fd62624b075e01de2
olingi <regnbogasulta@gmail.com>
2014-07-16 16:03:59 +0000

    geddem build breaker

--- SugarClient/src/com/jberry/dto/User.java
+++ SugarClient/src/com/jberry/dto/User.java
@@ -3,17 +3,10 @@ package com.jberry.dto;
 
 //impliments?
 public class User {
-<<<<<<< HEAD
     public String userName = "admin";
     public String password = "admin";
     public String email = "a@a.is";
     public boolean admin = true;
     public boolean isLoggedIn = false; //mabbeh óðarfi.
     public int Id;
-=======
-    String userName = "admin";
-    String password = "admin";
-    String email = "a@a.is";
-    int Id;
->>>>>>> parent of 5943663... made an artifact, dno wat to do wit et
 }
