f9d5e2a247d68a99f3db08f8e837296b933ec934
krissi92 <jayflug@gmail.com>
2014-07-22 17:27:37 +0000

    a brand new calandermock with unixseconds booyah

--- SugarClient/src/com/jberry/services/calander/CalanderServiceImpl.java
+++ SugarClient/src/com/jberry/services/calander/CalanderServiceImpl.java
@@ -7,34 +7,24 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.TimeZone;
+
 public class CalanderServiceImpl implements CalanderService {
 
     public List<CalanderMeal>getMealsByDay() {
-        SimpleDateFormat ft = new SimpleDateFormat("yyyy/MM/dd/HH");
-        String sampleDate = "2014/07/03/";
+        SimpleDateFormat ft = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss z");
+        long unixSeconds = 1404342000;
 
         List<CalanderMeal> mealList = new ArrayList<CalanderMeal>() {
         };
 
-        for(int i = 0; i<=24; i++) {
+        for(int i = 0; i<=23; i++) {
+            unixSeconds += 3600;
             CalanderMeal temp = new CalanderMeal();
-            String tString = "";
             temp.mealName = "BANANASALAT";
-            if (i < 10) {
-                tString = "0" + i;
-            } else {
-                tString = Integer.toString(i);
-            }
-            //Date d = ft.parse(sampleDate+tString);
-            String huh = sampleDate + tString;
-            Date d = null;
-            try {
-                d = ft.parse(huh);
-            } catch (ParseException e) {
-                e.printStackTrace();
-            }
+            Date d = new Date(unixSeconds*1000L);
+            ft.setTimeZone(TimeZone.getTimeZone("GMT"));
             temp.timeOfMeal = ft.format(d);
-
             temp.userId = 1;
             mealList.add(i, temp);
 
