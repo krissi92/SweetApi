1db67c401a3c44c5632f15bb6e019850037cfb3e
olingi <regnbogasulta@gmail.com>
2014-07-24 14:45:50 +0000

    Almennileg checkin Virkni kominn

--- SugarClient/src/com/jberry/services/checkin/CheckInService.java
+++ SugarClient/src/com/jberry/services/checkin/CheckInService.java
@@ -1,5 +1,7 @@
 package com.jberry.services.checkin;
 
+import java.util.Map;
+
 public interface CheckInService {
-    int calculateInsulin(double ratio, String foodName, int gramms, double bloodsuger, boolean hreyfing);
+    int calculateInsulin(double ratio, Map<String, Integer> foodMap, double bloodSugar, boolean exercise);
 }
--- SugarClient/src/is/jberry/runners/TestRunner.java
+++ SugarClient/src/is/jberry/runners/TestRunner.java
@@ -6,13 +6,21 @@ import com.jberry.services.calendar.CalendarServiceFactory;
 import com.jberry.services.checkin.CheckInService;
 import com.jberry.services.checkin.CheckInServiceFactory;
 
-import java.util.List;
+import java.util.*;
 
 public class TestRunner {
 
 	public static void main(String[] args) throws Exception {
         CheckInService checker = CheckInServiceFactory.getCheckInService();
-        int vampire = checker.calculateInsulin(5.5, "Banana", 200, 5.5, true);
+
+        Map<String, Integer> testMap = new HashMap<String, Integer>();
+        testMap.put("Ananas", 120);
+        testMap.put("Banani", 120);
+        testMap.put("Kiwi", 120);
+        testMap.put("Banani", 130);
+
+
+        int vampire = checker.calculateInsulin(5.5, testMap, 5.5, true);
         System.out.print("you need this amount of Blood " + vampire);
 	}
 
