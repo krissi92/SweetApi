661dc6dd5e1793f7b7ca469ae0f8c444f65a624b
olingi <regnbogasulta@gmail.com>
2014-07-28 16:25:18 +0000

    það sem gat verið private er nú private

--- SugarClient/src/com/jberry/dto/User.java
+++ SugarClient/src/com/jberry/dto/User.java
@@ -2,10 +2,60 @@ package com.jberry.dto;
 
 //TODO: make it private.
 public class User {
-    public String userName = "admin";
-    public String password = "admin";
-    public String email = "a@a.is";
-    public boolean admin = true;
-    public boolean isLoggedIn = false; //mabbeh óðarfi.
-    public int Id = 1337;
+    private String userName;
+    private String password;
+    private String email;
+    private boolean admin;
+    private boolean isLoggedIn;
+    private int Id;
+
+    //Constructor
+    public User(String userName, String password, String email, boolean admin, boolean isLoggedIn, int id) {
+        this.userName = "admin";
+        this.password = "admin";
+        this.email = "a@a.is";
+        this.admin = true;
+        this.isLoggedIn = false;
+        Id = 1337;
+    }
+
+    //Getters
+    public String getUserName() {
+        return userName;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public boolean isAdmin() {
+        return admin;
+    }
+    public boolean isLoggedIn() {
+        return isLoggedIn;
+    }
+    public int getId() {
+        return Id;
+    }
+
+    //Setters
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setAdmin(boolean admin) {
+        this.admin = admin;
+    }
+    public void setLoggedIn(boolean isLoggedIn) {
+        this.isLoggedIn = isLoggedIn;
+    }
+    public void setId(int id) {
+        Id = id;
+    }
 }
