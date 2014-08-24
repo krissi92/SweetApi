7f9beb5353445a99d677c7b8ad802f91b51ba3d1
krissi92 <jayflug@gmail.com>
2014-08-24 21:32:49 +0000

    fixed toolservice and userimpl a bit

--- SugarClient/src/com/jberry/services/tools/ToolService.java
+++ SugarClient/src/com/jberry/services/tools/ToolService.java
@@ -2,13 +2,16 @@ package com.jberry.services.tools;
 
 import com.jberry.dto.User;
 import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.codec.binary.StringUtils;
 
 public class ToolService {
     public String userEncoded(){
         User notandi = User.getTheUser();
         Base64 b64 = new Base64();
         String usr = notandi.getEmail() + ":" + notandi.getPassword();
-        return b64.encodeAsString(usr.getBytes());
+        String user = new String(b64.encodeBase64(StringUtils.getBytesUtf8(usr)));
+
+        return user;
     }
     public String url(){
         return "jberry.noip.me";
--- SugarClient/src/com/jberry/services/user/UserServiceImpl.java
+++ SugarClient/src/com/jberry/services/user/UserServiceImpl.java
@@ -35,7 +35,7 @@ public class UserServiceImpl implements UserService {
         request.setEntity(new UrlEncodedFormEntity(params,"UTF-8"));
         HttpResponse response = client.execute(request);
 
-        if(response.getStatusLine().getStatusCode() != 302){
+        if(response.getStatusLine().getStatusCode() != 200){
             return false;
         }
         User notandi = User.getTheUser();
